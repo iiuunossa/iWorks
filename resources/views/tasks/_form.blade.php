49ca9a1aeb306e7eb8def12570b6fdfa38ed2722
iiuunossa <karabuning_27@hotmail.com>
2019-09-02 17:48:35 +0700

    Create Form

--- resources/views/tasks/_form.blade.php
+++ resources/views/tasks/_form.blade.php
@@ -25,29 +25,45 @@
       </div> 
     @endif 
 
-    <div><h2>ข้อมูลภาระงาน</h2></div></br>
+    <div><h2>ข้อมูลภาระงาน</h2></div><hr></br>
         <div class="form-group row">
-            <label for="type" class="col-sm-2 col-form-label"><b>Job Type</b></label>
+            <label for="type" class="col-sm-2 col-form-label"><b>หมวดงาน : </b></label>
             <div class="col-sm-10">
                 <select class="form-control" id="type" name="type">
-                  <option value="" hidden select>เลือกประเภทงาน</option>
-                    <option value="1">Hardware</option>
-                    <option value="2">Software</option>
-                    <option value="3">Network</option>
-                    <option value="4">Virus</option>
-                    <option value="5">Consult</option>
+                  <option value="" hidden select>เลือกหมวดงาน</option>
+                    <option value="1">บริหารจัดการ</option>
+                    <option value="2">พัฒนาระบบ</option>
+                    <option value="3">MA</option>
+                    <option value="4">IT Support</option>
+                    <option value="5">ครุภัณฑ์</option>
                 </select>
             </div>
         </div>
 
+
     <div class="form-group row">
-      <label for="detail" class="col-sm-2 col-form-label"><b>Job Detail</b></label>
+      <label for="detail" class="col-lg-2 col-md-2 col-sm-2 col-form-lable"><b>รายละเอียดงาน : </b></label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="detail" name="detail">
       </div>
     </div>
+
     <div class="form-group row">
-      <label for="name" class="col-sm-2 col-form-label"><b>User</b></label>
+    <label for="detail" class="col-lg-2 col-md-2 col-sm-2 col-form-lable"><b>วัน/เวลาที่เริ่มต้น : </b></label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="detail" name="detail">
+    </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="detail" class="col-lg-2 col-md-2 col-sm-2 col-form-lable"><b>วัน/เวลาที่สิ้นสุด : </b></label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="detail" name="detail">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="name" class="col-sm-2 col-form-label"><b>เวลาทำงาน : </b></label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="name" name="name" value="{{old('name')}}">
       </div>
@@ -55,14 +71,16 @@
 
     <fieldset class="form-group">
       <div class="row">
-        <legend class="col-form-label col-sm-2 pt-0"><b>Status</b></legend>
+        <legend class="col-form-label col-sm-2 pt-0"><b>สถานะงาน : </b></legend>
         <div class="col-sm-10">
           <div class="form-check">
             <input class="form-check-input" type="radio" name="status" id="status1" value="0" checked>
             <label class="form-check-label" for="status1">
               On going
-            </label>
+            </label>  
+            
           </div>
+         
           <div class="form-check">
             <input class="form-check-input" type="radio" name="status" id="status2" value="1">
             <label class="form-check-label" for="status2">
@@ -73,8 +91,8 @@
       </div>
     </fieldset>
     
-    <div class="form-group row">
-      <div class="col-sm-4" align="center">
+    <div class="form-group row" align="center">
+      <div class="col-sm-5">
         <button type="submit" class="btn btn-primary">Save</button>
       </div>
     </div>
